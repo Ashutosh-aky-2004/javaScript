218be4ee30cbc8ae221d16502996f6d3c71b4286
Ashutosh-aky-2004 <151706966+Ashutosh-aky-2004@users.noreply.github.com>
2025-05-22 14:24:51 +0530

    Add files via upload

---
+++ Todo App/script.js
@@ -0,0 +1,81 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.querySelector('#todo-input');
+const todoListUL = document.querySelector('#todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0){
+        const todoObject = {
+            text: todoText,
+            completed: false,
+        };
+        allTodos.push(todoObject);
+        updateTodoList();
+        saveTodos();
+        todoInput.value = "";
+    }
+}
+
+function updateTodoList(){
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex)=>{
+        todoItem = createTodoItem(todo,todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo, todoIndex){
+    const todoId = "todo-" + todoIndex;
+    const todoLI = document.createElement("li");
+    todoLI.className = "todo";
+    const todoText = todo.text;
+    todoLI.innerHTML = `
+    
+     <input type="checkbox" id="${todoId}" />
+          <label class="custom-checkbox" for="${todoId}"
+            ><i class="fa-solid fa-check"></i>
+          </label>
+          <label class="todo-text" for="${todoId}"
+            >${todoText}
+          </label>
+          <button class="delete-button">
+            <i class="fa-solid fa-trash"></i>
+          </button>`;
+
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click",()=>{
+        deleteTodoItem(todoIndex);
+    })
+
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener('change',()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    });
+    checkbox.checked = todo.completed;
+    return todoLI;
+}
+
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_,i)=> i !== todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+
+function saveTodos(){
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos",todosJson);
+}
+
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
